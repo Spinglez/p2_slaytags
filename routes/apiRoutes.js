cb42de484d73b90d6cc36439282dcf99a94fe3c0
Spinglez <42229372+Spinglez@users.noreply.github.com>
2019-01-14 19:16:16 -0800

    fixed the link and refactored some code, took out link to JS on the index login page.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
-var db = require("../models");
+var db = require('../models');
 
 module.exports = function (app) {
-  app.get("/api/products", function (req, res) {
+  app.get('/api/products', function (req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
@@ -12,7 +12,7 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/api/products/:id", function (req, res) {
+  app.get('/api/products/:id', function (req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
@@ -26,7 +26,7 @@ module.exports = function (app) {
     });
   });
 
-  app.post("/api/products", function (req, res) {
+  app.post('/api/products', function (req, res) {
     db.Products.create(req.body).then(function (dbProducts) {
       res.json(dbProducts);
     });
