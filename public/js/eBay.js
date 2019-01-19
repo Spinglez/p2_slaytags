b6511cc697d5c69b3155b6dc15bd3e7947a8eda0
tam <taam.tr17@gmail.com>
2019-01-19 15:55:28 -0800

    Added table for product data

--- public/js/eBay.js
+++ public/js/eBay.js
@@ -6,7 +6,7 @@ const xmlString = xml(xmlObject, options);
 
 function addEbayProducts(userInput) {
 
-    queryURL = 'http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=XML&appid=TamTran-SlayTags-PRD-4392af54d-d1615769&siteid=0&version=967&QueryKeywords=iphone&AvailableItemsOnly=true&MaxEntries=3';
+    queryURL = 'http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=TamTran-SlayTags-PRD-4392af54d-d1615769&siteid=0&version=967&QueryKeywords=iphone&AvailableItemsOnly=true&MaxEntries=3';
     axios.get(queryURL).then(res => {
         console.log(res);
     });
@@ -21,3 +21,36 @@ function addEbayProducts(userInput) {
         console.log(results);
     });
 }
+
+function createProductRow(product) {
+    var newTr = $('<tr>');
+    newTr.data('product', product);
+    newTr.append('<td>' + product.title + '</td>');
+    newTr.append('<td>' + product.ProductID + '</td>');
+    newTr.append('<td>' + product.DetailsURL + '</td>');
+    newTr.append('<td>' + product.ItemSpecifics + '</td>');
+    newTr.append('<td>' + product.StockPhotoURL + '</td>');
+
+    return newTr;
+}
+
+function getProducts() {
+    return $.ajax({
+        // request to direct to app
+        url: '/api/products',
+        type: 'GET'
+    }).then(function (response) {
+        // store data in results
+        let results = response.data;
+
+        let rowsToAdd = [];
+        for (let i = 0; i < results.length; i++) {
+            rowsToAdd.push(createAuthorRow(results[i]));
+        }
+        // add rows through to handlebars
+        //  ...
+        nameInput.val('');
+    });
+}
+
+$('#search-item').on('click', getProducts());
---
+++ public/js/ebayTest.js
@@ -0,0 +1,6 @@
+{ "Timestamp": "2019-01-19T23:34:07.713Z", "Ack": "Success", "Build": "E1081_CORE_APILW2_18802887_R1", "Version": "1081", "ApproximatePages": 135772, "MoreResults": true, "PageNumber": 1, 
+"Product": [{ "DetailsURL": "http://www.ebay.com/p/1741918754#ProductDetails", "DisplayStockPhotos": false, 
+"ProductID": [{ "Value": "1741918754", "Type": "Reference" }, { "Value": "0754207825859", "Type": "UPC" }, { "Value": "0754207825859", "Type": "EAN" }],
+ "ItemSpecifics": { "NameValueList": [{ "Name": "Model", "Value": ["iPhone 5C"] }, { "Name": "Type", "Value": ["Display: LCD Screen", "Display: Screen Digitizer"] }, { "Name": "Compatible Model", 
+ "Value": ["For iPhone 5c", "For iPhone 5"] }, { "Name": "Color", "Value": ["Black"] }, { "Name": "Compatible Brand", "Value": ["For Apple"] }] }, 
+ "Title": "iPhone 5C Black LCD Screen Replacement with Precision Repair Kit" }], "TotalProducts": 135772 }
