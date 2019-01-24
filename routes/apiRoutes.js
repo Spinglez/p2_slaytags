603d1a644a362a1909e02ca8d35a2d56eae17882
Spinglez <42229372+Spinglez@users.noreply.github.com>
2019-01-23 17:58:00 -0800

    trying to clean up paths

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const db = require('../models');
-const bestBuy = require('../controllers/bestBuy');
-const eBay = require('../controllers/ebay');
+const bestBuy = require('./../controllers/bestBuy');
+const eBay = require('./../controllers/ebay');
 
 module.exports = function (app) {
   app.get('/api/products', function (req, res) {
